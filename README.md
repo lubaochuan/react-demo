3fe5006e73d5a58a5846ef67cd50b7a222f23dda
lubaochuan <lubaochuan@gmail.com>
2017-05-17 16:53:19 -0500

    update UI with timer and see React difference

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-demo
+
+example from https://medium.freecodecamp.com/yes-react-is-taking-over-front-end-development-the-question-is-why-40837af8ab76
--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 console.log("hello world!")
 
 const jsContainer = document.getElementById("js")
-jsContainer.innerHTML = `
-  <div class="demo">
-    Hello JS
-    <input />
-    <p>${new Date()}</p>
-  </div>`
-
 const reactContainer = document.getElementById("react")
-ReactDOM.render(
-  React.createElement(
-    "div",
-    { className: "demo" },
-    "Hello React",
-    React.createElement("input"),
+
+const render = () => {
+  jsContainer.innerHTML = `
+    <div class="demo">
+      Hello JS
+      <input />
+      <p>${new Date()}</p>
+    </div>
+  `;
+  ReactDOM.render(
     React.createElement(
-      "p",
-      null,
-      new Date().toString()
-    )
-  ),
-  reactContainer
-)
+      "div",
+      { className: "demo" },
+      "Hello React ",
+      React.createElement("input"),
+      React.createElement(
+        "p",
+        null,
+        new Date().toString()
+      )
+    ),
+    reactContainer
+  );
+}
+
+setInterval(render, 1000);
